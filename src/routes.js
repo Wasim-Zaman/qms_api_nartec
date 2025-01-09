3087b94b3308dcc1c919bff62a7e53b61f0f8b19
Wasim Zaman <wasimxaman13@gmail.com>
2025-01-09 09:54:32 +0500

    Refactor routes.js to use userRoutes instead of superadminRoutes Delete superadmin.prisma and superAdmin.js Add Patient model to prisma/schema/patient.prisma Update userId data type in refreshToken.prisma Delete user.prisma Update user.js routes to remove unnecessary routes Update user.schema.js to match the new user schema

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
 import express from "express";
 
-import superadminRoutes from "./routes/superadmin.js";
+import userRoutes from "./routes/user.js";
 
 const router = express.Router();
 
-router.use("/admin", superadminRoutes);
+router.use("/user", userRoutes);
 
 export default router;
