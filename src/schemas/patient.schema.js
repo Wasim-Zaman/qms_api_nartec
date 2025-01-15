6963469efc2fa53562c6a5edc177c38e3349d94a
Wasim Zaman <wasimxaman13@gmail.com>
2025-01-15 13:17:33 +0500

    Refactor patient.schema.js: Update patient sex and state validation

--- src/schemas/patient.schema.js
+++ src/schemas/patient.schema.js
@@ -1,11 +1,15 @@
 import Joi from "joi";
 
 export const patientStatus = ["Non-urgent", "Urgent", "Critical"];
+export const patientState = [0, 1, 2];
+export const patientSex = ["M", "F", "O"];
 
 export const createPatientSchema = Joi.object({
   name: Joi.string().required(),
   nationality: Joi.string().allow(null, ""),
-  sex: Joi.string().valid("M", "F", "O").allow(null, ""),
+  sex: Joi.string()
+    .valid(...patientSex)
+    .allow(null, ""),
   idNumber: Joi.string().allow(null, ""),
   age: Joi.number().integer().min(0).allow(null),
   mobileNumber: Joi.string().allow(null, ""),
@@ -13,20 +17,28 @@ export const createPatientSchema = Joi.object({
   status: Joi.string()
     .valid(...patientStatus)
     .default("Non-urgent"),
-  state: Joi.number().integer().min(0).max(2).default(0),
+  state: Joi.number()
+    .integer()
+    .valid(...patientState)
+    .default(0),
   callPatient: Joi.boolean().default(false),
 });
 
 export const updatePatientSchema = Joi.object({
   name: Joi.string(),
   nationality: Joi.string().allow(null, ""),
-  sex: Joi.string().valid("M", "F", "O").allow(null, ""),
+  sex: Joi.string()
+    .valid(...patientSex)
+    .allow(null, ""),
   idNumber: Joi.string().allow(null, ""),
   age: Joi.number().integer().min(0).allow(null),
   mobileNumber: Joi.string().allow(null, ""),
   cheifComplaint: Joi.string().allow(null, ""),
   status: Joi.string().valid(...patientStatus),
-  state: Joi.number().integer().min(0).max(2).allow(null, ""),
+  state: Joi.number()
+    .integer()
+    .valid(...patientState)
+    .allow(null, ""),
   callPatient: Joi.boolean().allow(null, ""),
 });
 
