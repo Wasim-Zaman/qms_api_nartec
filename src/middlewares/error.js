40f9513a695f94987524673ac34b3c888581b937
Wasim Zaman <wasimxaman13@gmail.com>
2025-02-04 22:17:08 +0500

    Update error handler default message to be more concise

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -10,8 +10,7 @@ export const notFoundHandler = (req, res, next) => {
 export const errorHandler = (error, req, res, next) => {
   console.log(error);
   let status = 500;
-  let message =
-    "An error occurred while processing your request. Please try again later.";
+  let message = "Internal server error";
   let data = null;
   let success = false;
 
